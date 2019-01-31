fce5c15b59452eb4cf225d7b592e5b9984762866
Sharan <Sharanjeetk@packt.com>
2019-01-31 13:39:14 +0530

    code pushed
    code pushed

---
+++ Section 3/AddAndRemoveRoles.ps1
@@ -0,0 +1,17 @@
+﻿# Get the list of installed and available Roles and Features
+Get-WindowsFeature
+
+# Get the Web-Server Role
+Get-WindowsFeature -Name Web-Server
+
+# Install the Web-Server Role
+Add-WindowsFeature -Name Web-Server
+
+# Verify that the Web-Server Role has been installed
+Get-WindowsFeature -Name Web-Server
+
+# Remove the Web-Server Role
+Remove-WindowsFeature -Name Web-Server 
+
+# Verify that the Web-Server Role has been removed
+Get-WindowsFeature -Name Web-Server
